90084fd2abca953bace91f66c0bf58295072a0c9
kaAR17 <vanithasure14@gmail.com>
2024-12-18 15:15:04 +0530

    update files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var menuicon=document.getElementById("menuicon")
 var closenav=document.getElementById("closenav")
 
 menuicon.addEventListener("click",function(){
-    sidenav.style.right=0
+    sidenav.style.right="0"
 })
 closenav.addEventListener("click",function(){
     sidenav.style.right="-50%"
